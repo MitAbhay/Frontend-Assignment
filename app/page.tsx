d35b361ffc30e487f3307120e758a56c5b656569
mitabhay <leader.abhay002@gmail.com>
2024-04-23 01:27:06 +0530

    index route fixed

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,18 @@
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import Metrics from "./metrics/[timestamp]/page";
+import DefaultLayout from "@/components/DefaultLayout";
 
 export default function Home() {
-  const router = useRouter();
-  router.push("/metrics/300000");
+  // const router = useRouter();
+  // router.push("/metrics/300000");
 
   return (
     <>
-      <Metrics />
+      <DefaultLayout>
+        <div>Home Page</div>
+        <div>Created By : Abhay Kumar Mittal</div>
+      </DefaultLayout>
     </>
   );
 }
